20913fd5eed1804e4262a39e0f8424e03320e957
Umadevi695 <uma970179@gmail.com>
2025-05-27 19:10:29 +0530

    updated

---
+++ src/components/App19.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+function App19() {
+  const [run, setRun] = useState(0);
+  const [wicket, setWicket] = useState(0);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (wicket >= 10) {
+      setMessage("Game Over!");
+    }
+  }, [wicket]);
+
+  const handleRun = () => {
+    if (wicket < 10) {
+      setRun(run + 1);
+      setMessage("Well Done!");
+    }
+  };
+
+  const handleWicket = () => {
+    if (wicket < 10) {
+      setWicket(wicket + 1);
+      setMessage("Better Luck Next Time!");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h2>Cricket Scoreboard</h2>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "50px",
+          margin: "30px",
+        }}
+      >
+        <div>
+          <h3>{run}</h3>
+          <button onClick={handleRun}>Run</button>
+          <h3>{wicket}</h3>
+          <button onClick={handleWicket}>Wicket</button>
+        </div>
+
+        <div>
+          <h3>{message}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App19;
